a3532bad9c2c5b8aa83545bc1d3652192f011b89
Madison Morris <madsmorris14@gmail.com>
2018-05-13 20:20:17 -0400

    filesystem

---
+++ app/data/friends.js
@@ -0,0 +1,107 @@
+var friends = [
+
+	{
+		name: "Mapi",
+		photo: "http://www.ywcabrand.org/atf/cf/%7Bc3201f87-9e48-4580-84fa-429e77844aa1%7D/BLP0062303.JPG",
+		scores: [
+			"3",
+			"4",
+			"5",
+			"2",
+			"3",
+			"1",
+			"3",
+			"4",
+			"3",
+			"2",
+		]
+	},
+
+	{
+		name: "Lauren",
+		photo: "http://www.ywcabrand.org/atf/cf/%7Bc3201f87-9e48-4580-84fa-429e77844aa1%7D/BLP0125028.JPG",
+		scores: [
+			"3",
+			"4",
+			"5",
+			"3",
+			"2",
+			"1",
+			"1",
+			"3",
+			"4",
+			"5",
+		]
+	},
+
+	{
+		name: "Jackie",
+		photo: "http://www.ywcabrand.org/atf/cf/%7Bc3201f87-9e48-4580-84fa-429e77844aa1%7D/CBP1054044.JPG",
+		scores: [
+			"2",
+			"3",
+			"4",
+			"2",
+			"3",
+			"1",
+			"4",
+			"5",
+			"2",
+			"4",
+		]
+	},
+
+	{
+		name: "Madison",
+		photo: "http://www.ywcabrand.org/atf/cf/%7Bc3201f87-9e48-4580-84fa-429e77844aa1%7D/FAN9002429.JPG",
+		scores: [
+			"4",
+			"3",
+			"2",
+			"4",
+			"5",
+			"2",
+			"1",
+			"3",
+			"4",
+			"5",
+		]
+	},
+
+	{
+		name: "Martha",
+		photo: "http://www.ywcabrand.org/atf/cf/%7Bc3201f87-9e48-4580-84fa-429e77844aa1%7D/FAN9010365.JPG",
+		scores: [
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"4",
+			"5",
+			"1",
+		]
+	},
+
+	{
+		name: "Gretel",
+		photo: "http://www.ywcabrand.org/atf/cf/%7Bc3201f87-9e48-4580-84fa-429e77844aa1%7D/GETTYIMAGES-184837236.JPG",
+		scores: [
+			"2",
+			"3",
+			"3",
+			"4",
+			"4",
+			"5",
+			"2",
+			"5",
+			"4",
+			"2",
+		]
+	}
+
+];
+
+module.exports = friends;
